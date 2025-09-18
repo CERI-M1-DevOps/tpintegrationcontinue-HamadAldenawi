0f7d149459ccc45095914adeac8b8c1600330a85
uapv2603127 <uapv2603127@e-mc128-09.ad.univ-avignon.fr>
2025-09-18 19:23:09 +0200

    push

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ListeSimpleTest {
+public class ListeSimpleTest {
 
     ListeSimple listeATester;
 
     @BeforeEach
-    void initialiser() {
+    public void init() {
         listeATester = new ListeSimple();
     }
 
