72224b04715048c0a17257e35e2f881610109ab6
HamadAldenawi <hamadaldenawi@gmail.com>
2025-09-18 22:38:59 +0200

    push

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -113,11 +113,14 @@ class ListeSimpleTest {
     }
 
     @Test
-    void supprimeTousListeVide() {
-        listeATester.supprimePremier(1);
-        assertNull(listeATester.tete);
-        assertEquals(0, listeATester.getSize());
-    }
+   @Test
+void supprimeTousListeVide() {
+    // Call supprimeTous instead of supprimePremier
+    listeATester.supprimeTous(1);
+    assertNull(listeATester.tete);
+    assertEquals(0, listeATester.getSize());
+}
+
 
     @Test
     void supprimeTousUneSeuleFoisAuDebut() {
