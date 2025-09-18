d0a172186cf4c61a7119007bc554b81d2ab849fa
uapv2603127 <uapv2603127@e-mc128-09.ad.univ-avignon.fr>
2025-09-18 18:49:31 +0200

    push

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -122,6 +122,7 @@ public class ListeSimple {
             return;
         Noeud precedentR1;
         Noeud precedentR2;
+        
         if (r1 != tete && r2 != tete) {
             precedentR1 = getPrecedent(r1);
             precedentR2 = getPrecedent(r2);
